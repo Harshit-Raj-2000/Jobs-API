2d522aa8cb8f5f6e5c427de10687c1588a13ca15
Harshit-Raj-2000 <52253887+Harshit-Raj-2000@users.noreply.github.com>
2021-11-25 08:32:10 +0530

    added a few security packages

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')//adds security headers to protect site
+const cors = require('cors')  //makes the resources from this server accessible to other domains(front end apps)
+const xss = require('xss-clean') //santises the user input to prevent cross site scripting attacks
+const rateLimiter = require('express-rate-limit') // limits the num of requests a user can send
+
 const express = require('express');
 const app = express();
 
@@ -18,7 +25,16 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+app.set('trust proxy', 1) //required to be set if behind a reverse proxy like heroku
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minute
+  max: 100, // limit each IP to 100 requests per windowMs
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
